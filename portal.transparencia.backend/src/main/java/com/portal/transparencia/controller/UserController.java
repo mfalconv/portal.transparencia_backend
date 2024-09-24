adf33e13ebf984f97e9c4a7045e20c41caaffd81
Mike <mfalconv1@upao.edu.pe>
2024-09-24 12:34:01 -0500

    Signed-off-by: Mike <mfalconv1@upao.edu.pe>

--- portal.transparencia.backend/src/main/java/com/portal/transparencia/controller/UserController.java
+++ portal.transparencia.backend/src/main/java/com/portal/transparencia/controller/UserController.java
@@ -3,17 +3,22 @@ package com.portal.transparencia.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.portal.transparencia.exceptions.ResourceNotFoundException;
 import com.portal.transparencia.model.User;
 import com.portal.transparencia.repository.UserRepository;
 
 @RestController
 @RequestMapping ("/api/v1/")
+// @CrossOrigin(origins= "") -> añadir aqui la ruta del front para intercambiar servicios
 public class UserController {
 	
 	
@@ -24,11 +29,15 @@ public class UserController {
 	public List<User> listallUsers(){
 		return repositorio.findAll();
 		}
-	
 	@PostMapping("/usuarios")
 	public User saveUser(@RequestBody User user) {
 		return repositorio.save(user);
+	}
+	@GetMapping("/usuarios/{id}")
+	public ResponseEntity<User> getUserByID(@PathVariable Long id){
+		User user= repositorio.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("No existe el empleado con el ID: "+ id));
+		return ResponseEntity.ok(user);
 		
 	}
-	
 }
--- portal.transparencia.backend/src/main/java/com/portal/transparencia/model/User.java
+++ portal.transparencia.backend/src/main/java/com/portal/transparencia/model/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "usuarios")
@@ -24,48 +25,135 @@ public class User {
 	@Column(name = "role", length = 20)
 	private String role;
 
-	public User() {
+	@Column(name = "purchaseDate", nullable=false)
+	private LocalDate purchaseDate;
+	
+	@Column(name="parcel_number", nullable = true, unique=true, length=50)
+	private String parcelNumber; 
+	
+	@Column(name="payment", nullable=true, length=20)
+	private String payment; 
+	
+	@Column(name="sign", nullable = false)
+	private Boolean sign; 
+	
+	
 
+	public User() {
+		super();
 	}
 
-	public User(Long id, String email, String password, String role) {
+
+
+	public User(Long id, String email, String password, String role, LocalDate purchaseDate, String parcelNumber,
+			String payment, Boolean sign) {
 		super();
 		this.id = id;
 		this.email = email;
 		this.password = password;
 		this.role = role;
+		this.purchaseDate = purchaseDate;
+		this.parcelNumber = parcelNumber;
+		this.payment = payment;
+		this.sign = sign;
 	}
+
+
 
 	public Long getId() {
 		return id;
 	}
 
+
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+
 
 	public String getEmail() {
 		return email;
 	}
 
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+
 
 	public String getPassword() {
 		return password;
 	}
 
+
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+
 
 	public String getRole() {
 		return role;
 	}
 
+
+
 	public void setRole(String role) {
 		this.role = role;
 	}
+
+
+
+	public LocalDate getPurchaseDate() {
+		return purchaseDate;
+	}
+
+
+
+	public void setPurchaseDate(LocalDate purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+
+
+	public String getParcelNumber() {
+		return parcelNumber;
+	}
+
+
+
+	public void setParcelNumber(String parcelNumber) {
+		this.parcelNumber = parcelNumber;
+	}
+
+
+
+	public String getPayment() {
+		return payment;
+	}
+
+
+
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+
+
+
+	public Boolean getSign() {
+		return sign;
+	}
+
+
+
+	public void setSign(Boolean sign) {
+		this.sign = sign;
+	}
+
+	
 
 }
